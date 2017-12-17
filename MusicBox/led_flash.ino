80e917551077c674477916de8b42ef4c9c7579cb
issac <issac@hexenring.net>
2017-12-17 17:05:11 -0500

    Add files via upload

---
+++ MusicBox/led_flash.ino
@@ -0,0 +1,19 @@
+#define PIN_SPEAKER       9
+#define PIN_BUTTON        8
+#define PIN_BUILTIN_LED  13
+
+
+void setup(){
+  pinMode(PIN_BUILTIN_LED, OUTPUT);
+  pinMode(PIN_BUTTON, INPUT_PULLUP);
+}
+
+
+void loop(){
+
+  if (digitalRead(PIN_BUTTON) == LOW){
+      digitalWrite(PIN_BUILTIN_LED, HIGH);
+      delay(500);
+      digitalWrite(PIN_BUILTIN_LED, LOW);
+  }
+}
---
+++ MusicBox/led_flash_beep.ino
@@ -0,0 +1,25 @@
+
+#define PIN_SPEAKER       9
+#define PIN_BUTTON        8
+#define PIN_BUILTIN_LED  13
+
+
+void setup(){
+  pinMode(PIN_BUILTIN_LED, OUTPUT);
+  pinMode(PIN_SPEAKER, OUTPUT);
+  pinMode(PIN_BUTTON, INPUT_PULLUP);
+}
+
+
+void loop(){
+
+  if (digitalRead(PIN_BUTTON) == LOW){
+      digitalWrite(PIN_BUILTIN_LED, HIGH);
+      tone(PIN_SPEAKER,1000);
+      delay(500);
+      noTone(PIN_SPEAKER);
+      digitalWrite(PIN_BUILTIN_LED, LOW);
+  }
+}
+
+
